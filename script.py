034c3df91817bc3b2f1b628719c8dbd1cdd48682
Yarlagadda <SYARLAG1@cstcis.cti.depaul.edu>
2016-07-26 18:43:35 -0500

    rf and lda probabilistic outputs generated

--- script.py
+++ script.py
@@ -67,16 +67,20 @@ train = combined_raw_encoded.iloc[:train_raw.shape[0],:]
 test = combined_raw_encoded.iloc[train_raw.shape[0]:combined_raw.shape[0],:]
 
 from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
 
 zscore_fit = StandardScaler().fit(train)
 train_normalized = zscore_fit.transform(train)
 test_normalized = zscore_fit.transform(test)
 
+minmax_fit = MinMaxScaler().fit(train)
+train_standardized = minmax_fit.transform(train)
+test_standardized = minmax_fit.transform(test)
+
 from sklearn.cross_validation import train_test_split 
 
 X_train, X_test, y_train, y_test = train_test_split(train, target, test_size=0.33, random_state=42)
-
-from sklearn.metrics import accuracy_score, confusion_matrix
+X_train, X_test, y_train, y_test = train_test_split(train_standardized, target, test_size=0.33, random_state=42)
 
 ########################First try is without train test split#####################
 from sklearn.ensemble import AdaBoostClassifier as ad
@@ -84,6 +88,12 @@ from sklearn.ensemble import RandomForestClassifier as rf
 from sklearn.naive_bayes import GaussianNB
 from sknn.mlp import Classifier, Layer
 from sklearn import svm
+from sklearn.metrics import accuracy_score, confusion_matrix, recall_score
+from sklearn.lda import LDA
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+
 
 nn = Classifier(
     layers=[Layer("Softmax")],
@@ -92,22 +102,76 @@ nn = Classifier(
 nn.fit(train_normalized, target)
 y_predict = nn.predict(test_normalized)
 
-ad_fit = ad(n_estimators = 10).fit(train,target)
-y_pred = ad_fit.predict(test)
+ad_fit = ad(n_estimators = 10).fit(X_train,y_train)
+y_pred = ad_fit.predict(X_test)
+ad_acc = accuracy_score(y_pred, y_test) #0.60
 
-y_pred = rf().fit(train,target).predict(test)
+y_pred = rf().fit(X_train,y_train).predict(X_test)
+rf_acc = accuracy_score(y_pred, y_test) #0.59
 
-gnb = GaussianNB()
-y_pred = gnb.fit(train, target).predict(test)
+gnb = GaussianNB() 
+y_pred = gnb.fit(X_train, y_train).predict(X_test)
+gnb_acc = accuracy_score(y_pred, y_test) #0.075 (extremely low)
+
+svc = svm.SVC()
+svc = svc.fit(X_train, y_train)
+y_pred = svc.predict(X_test)
+svc_acc = accuracy_score(y_pred, y_test)
 
 
-clf = svm.SVC()
-clf = clf.fit(X_train, y_train)
+ldaC = LDA().fit(X_train, y_train)
+y_pred = ldaC.predict(X_test)
+lda_acc = accuracy_score(y_pred, y_test) #0.62
+
+confusionMat=confusion_matrix(y_pred, y_test)
+
+recalls = []
+totals = []
+predictionCounts = []
+
+for i in range(len(confusionMat)):
+    recalls.append(float(confusionMat[i,i])/np.sum(confusionMat[i]))
+    totals.append(np.sum(confusionMat[:,i]))
+    predictionCounts.append(np.sum(confusionMat[i]))
+
+
+logreg = LogisticRegression().fit(X_train, y_train)
+y_pred = logreg.predict(X_test)
+y_pred_train = logreg.predict(X_train)
+log_acc =  accuracy_score(y_pred, y_test) #0.63 highest
+
+clf = DecisionTreeClassifier().fit(X_train, y_train)
 y_pred = clf.predict(X_test)
+clf_acc =  accuracy_score(y_pred, y_test) #0.61 
+
+neigh = KNeighborsClassifier(n_neighbors=13).fit(X_train, y_train)
+y_pred = neigh.predict(X_test)
+nn_acc =  accuracy_score(y_pred, y_test) #0.61
+
+########FOCUSING ON LOGISTIC REGRESSION AND LDA TEST DATA########################
+from sklearn.linear_model import LogisticRegressionCV
+
+logregCV = LogisticRegressionCV(cv= 10, solver = 'lbfgs', penalty = 'l2').fit(train_standardized, target)
+logCV_acc = logregCV.scores_
+y_pred = logregCV.predict_proba(test_standardized)
+
+
+ldaC = LDA().fit(train_standardized, target)
+y_pred = ldaC.predict_proba(test_standardized)
+
+ad_fit = ad(n_estimators = 10).fit(train_standardized, target)
+y_pred = ad_fit.predict_proba(test_standardized)
+
+rf_fit = rf().fit(train_standardized, target)
+y_pred = rf_fit.predict_proba(test_standardized)
+
+
 
 
 ################SUBMISSION#################
 
-pred_df = pd.DataFrame(y_pred); submission_format = pd.get_dummies(pred_df); 
-submission_format.columns = ['Adoption','Died','Euthanasia','Return_to_owner','Transfer']
-submission_format.to_csv('./submission.csv')
+pred_df = pd.DataFrame(y_pred); #submission_format = pd.get_dummies(pred_df); 
+pred_df.columns = ['Adoption','Died','Euthanasia','Return_to_owner','Transfer']
+pred_df.index += 1
+pred_df.index.rename('ID', inplace=True)
+pred_df.to_csv('./submission.csv')
